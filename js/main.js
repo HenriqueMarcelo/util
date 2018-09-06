f5d95bc3e3d8c732298c84fc42d16935d750d586
Henrique <henrique@designtec.com.br>
2018-09-06 11:55:09 -0300

    Correção do bug na transformação de time para data

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
+/*
+ * Recebe um timestamp em milisegundos da era Unix
+ * Retorna uma string representando sua data
+ */
 function timeToString(timestamp){
   var a = new Date(parseInt(timestamp));
   return a.toLocaleString();
 }
 
+/*
+ * Recebe uma string de data em formato de máquina
+ * Retorna o timestamp em milisegundos dessa data
+ */
 function dateToTimestamp(date){
   var a = new Date(date.split('-'));
   return a.getTime();
 };
 
+/*
+ * Recebe os 3 números dos inputs
+ * Retorna o resultado
+ */
 function calculaRegraTres(a,b,x){
   if(a && b && x){
     return (x * b) / a;
@@ -16,20 +28,26 @@ function calculaRegraTres(a,b,x){
   }
 }
 
+/*
+ *  Inputs
+ */
 var inputTime = document.getElementsByClassName('input-time')[0];
 var inputDate = document.getElementsByClassName('input-data')[0];
 var inputA    = document.getElementsByClassName('input-a')[0];
 var inputB    = document.getElementsByClassName('input-b')[0];
 var inputX    = document.getElementsByClassName('input-x')[0];
 
-var datePHP = document.getElementsByClassName('date-php')[0];
-var dateJS  = document.getElementsByClassName('date-js')[0];
-var timePHP = document.getElementsByClassName('time-php')[0];
-var timeJS  = document.getElementsByClassName('time-js')[0];
-var regraTres  = document.getElementsByClassName('regra-tres')[0];
+/*
+ *  Respostas
+ */
+var datePHP   = document.getElementsByClassName('date-php')[0];
+var dateJS    = document.getElementsByClassName('date-js')[0];
+var timePHP   = document.getElementsByClassName('time-php')[0];
+var timeJS    = document.getElementsByClassName('time-js')[0];
+var regraTres = document.getElementsByClassName('regra-tres')[0];
 
 function callBackInputTime(){
-  datePHP.innerHTML = timeToString(inputTime.value / 1000);
+  datePHP.innerHTML = timeToString(inputTime.value * 1000);
   dateJS.innerHTML = timeToString(inputTime.value);
 };
 function callBackInputDate(){
@@ -44,6 +62,8 @@ function callBackRegraTres(){
   );
 }
 
+//
+
 inputTime.addEventListener("change", callBackInputTime);
 inputTime.addEventListener("keyup", callBackInputTime);
 
@@ -51,9 +71,13 @@ inputTime.addEventListener("keyup", callBackInputTime);
 inputDate.addEventListener("change", callBackInputDate);
 inputDate.addEventListener("keyup", callBackInputDate);
 
+//
+
 inputA.addEventListener("change", callBackRegraTres);
 inputA.addEventListener("keyup", callBackRegraTres);
+
 inputB.addEventListener("change", callBackRegraTres);
 inputB.addEventListener("keyup", callBackRegraTres);
+
 inputX.addEventListener("change", callBackRegraTres);
 inputX.addEventListener("keyup", callBackRegraTres);
